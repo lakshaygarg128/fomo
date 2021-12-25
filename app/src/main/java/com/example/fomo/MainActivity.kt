870afbe17ae88387f629b4223b4cf1f5b00d0ce1
lakshaygarg128 <lakshaygarg128@gmail.com>
2021-12-25 18:15:15 +0530

    error fixed at some extent navigation bottom

--- app/src/main/java/com/example/fomo/MainActivity.kt
+++ app/src/main/java/com/example/fomo/MainActivity.kt
@@ -1,42 +1,37 @@
 package com.example.fomo
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.darwin.physioai.coreapp.utils.NetworkUtil
 import com.example.fomo.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bottomNavigationView: BottomNavigationView
+
     private var binding : ActivityMainBinding? = null
-    private lateinit var navController : NavController
-    private lateinit var networkUtil: NetworkUtil
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+      //  binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-
-        replaceFragment(ProfileFragment())
-
+       // replaceFragment(Mood())
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId) {
-                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.menu_profile -> replaceFragment(ProfileFragment())
                 R.id.weather -> replaceFragment(WeatherFragment())
-                R.id.mood -> replaceFragment(Mood())
-                R.id.location -> replaceFragment(LocationFragment())
+                R.id.menu_mood -> replaceFragment(Mood())
+                R.id.menu_location-> replaceFragment(LocationFragment())
             }
             true
         }
